e130c20670c7b83088b537715c623a5b85bba6bc
ChumiV <38505086+ChumiV@users.noreply.github.com>
2021-11-02 14:41:48 +0900

    Create Answer.cs

---
+++ Models/Answer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMudBlazor.Models
+{
+    public class Answer
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public bool IsTrue { set; get; }
+    }
+}
